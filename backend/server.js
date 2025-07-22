f76768ee16e8b363187d4d7d543f9728669fcfb2
PoPoVvVvV <48895695+PoPoVvVvV@users.noreply.github.com>
2025-07-22 23:54:21 +0200

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -13,38 +13,35 @@ import settingRoutes from './routes/settings.js';
 import userRoutes from './routes/users.js';
 import recipeRoutes from './routes/recipes.js';
 
-// Charger les variables d'environnement depuis le fichier .env
 dotenv.config();
 
-// Initialiser l'application Express
 const app = express();
 
-// --- Middleware ---
-// Autorise les requêtes provenant d'autres origines (notre frontend React)
-app.use(cors());
-// Permet au serveur de comprendre et de traiter les données JSON envoyées dans les requêtes
+// --- Configuration CORS pour la Production ---
+// Remplacez l'URL ci-dessous par celle de votre site Vercel
+const corsOptions = {
+  origin: 'https://restaurant-app-coral-six.vercel.app', 
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+// ---
+
 app.use(express.json());
 
-// --- Connexion à la base de données MongoDB ---
 const connectDB = async () => {
   try {
-    // Tente de se connecter en utilisant l'URI stockée dans les variables d'environnement
     await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Connexion à MongoDB réussie !');
   } catch (error) {
-    // Affiche l'erreur en cas d'échec et arrête le processus du serveur
     console.error('❌ Erreur de connexion à MongoDB :', error.message);
     process.exit(1);
   }
 };
 
-// Exécuter la fonction de connexion à la base de données
 connectDB();
 
 // --- Définition des Routes de l'API ---
-// Toutes les routes définies dans `auth.js` seront préfixées par `/api/auth`
 app.use('/api/auth', authRoutes);
-// Toutes les routes définies dans `products.js` seront préfixées par `/api/products`
 app.use('/api/products', productRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/expenses', expenseRoutes);
@@ -53,13 +50,10 @@ app.use('/api/settings', settingRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/recipes', recipeRoutes);
 
-// Route de test simple pour la racine de l'API
 app.get('/', (req, res) => {
   res.send('API du restaurant en cours d\'exécution...');
 });
 
-// Définir le port d'écoute du serveur
 const PORT = process.env.PORT || 5000;
 
-// Démarrer le serveur et écouter les requêtes sur le port défini
 app.listen(PORT, () => console.log(`🚀 Serveur démarré sur le port ${PORT}`));
