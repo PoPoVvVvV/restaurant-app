777a4e6de61b3a481b2671c43ab9735b76431e05
PoPoVvVvV <48895695+PoPoVvVvV@users.noreply.github.com>
2025-07-23 17:33:02 +0200

    Update the API URL to use the environment variable REACT_APP_API_URL

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -34,7 +34,7 @@ function RegisterPage() {
 
     try {
       const API_URL = process.env.REACT_APP_API_URL || 'https://restaurant-app-production-61c2.up.railway.app/api/auth/register';
-      const response = await axios.post(API_URL, formData);
+      const response = await axios.post(REACT_APP_API_URL, formData);
       setSuccess(response.data.message);
 
       setTimeout(() => {
