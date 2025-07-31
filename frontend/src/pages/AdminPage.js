4b09268ec253cf06265a9ef0f2c4a00a4d6eda6f
PoPoVvVvV <48895695+PoPoVvVvV@users.noreply.github.com>
2025-07-31 22:09:13 +0200

    Fix useMemo import in AdminPage.js

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import api from '../services/api';
 import { useNotification } from '../context/NotificationContext';
 
