e1b32ff28082da23bb6014c13b6d75364a4c8880
PoPoVvVvV <48895695+PoPoVvVvV@users.noreply.github.com>
2025-12-02 11:34:35 +0100

    Patch Error

--- frontend/src/components/Snowflakes.js
+++ frontend/src/components/Snowflakes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useMemo } from 'react';
 import { keyframes, styled } from '@mui/material/styles';
 
 const createFallAnimation = (drift) => keyframes`
